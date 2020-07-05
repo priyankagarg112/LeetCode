4dbe26220b9ff97c71c3adf20da88214ea66a6f0
Priyanka Garg <priyankagarg112@gmail.com>
2020-07-05 18:48:34 +0530

    solved hamming distnace problem

---
+++ JulyChallenge/HammingDistance.py
@@ -0,0 +1,31 @@
+'''
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+'''
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        z= x^y
+        temp = []
+        while z:
+            temp.append(z %2)
+            z=z//2
+
+        return sum(temp)
